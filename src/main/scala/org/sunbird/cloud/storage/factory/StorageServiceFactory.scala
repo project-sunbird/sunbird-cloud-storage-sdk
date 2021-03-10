de789ea3b1394d22a0cd0c30383d9192fda7690d
Sunny Jaiswal <sunny.jaiswal@nic.in>
2021-03-10 15:38:42 +0530

    Ceph storage support

--- src/main/scala/org/sunbird/cloud/storage/factory/StorageServiceFactory.scala
+++ src/main/scala/org/sunbird/cloud/storage/factory/StorageServiceFactory.scala
@@ -2,9 +2,9 @@ package org.sunbird.cloud.storage.factory
 
 import org.sunbird.cloud.storage.BaseStorageService
 import org.sunbird.cloud.storage.exception.StorageServiceException
-import org.sunbird.cloud.storage.service.{AzureStorageService, S3StorageService}
+import org.sunbird.cloud.storage.service.{AzureStorageService, S3StorageService, CephS3StorageService}
 
-case class StorageConfig(`type`: String, storageKey: String, storageSecret: String)
+case class StorageConfig(`type`: String, storageKey: String, storageSecret: String, endPoint: Option[String] = None)
 
 object StorageServiceFactory {
 
@@ -19,6 +19,8 @@ object StorageServiceFactory {
                 new S3StorageService(config);
             case "azure"   =>
                 new AzureStorageService(config);
+            case "cephs3"      =>
+                new CephS3StorageService(config);
             case _         =>
                 throw new StorageServiceException("Unknown storage type found");
         }
