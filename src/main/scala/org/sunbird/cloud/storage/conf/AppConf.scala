5e418152a79d622bb4d6d9a55f147a87bf228d99
karthik-tarento <karthikeyan.rajendran@tarento.com>
2022-04-05 16:15:05 +0530

    Cephs3 changes

--- src/main/scala/org/sunbird/cloud/storage/conf/AppConf.scala
+++ src/main/scala/org/sunbird/cloud/storage/conf/AppConf.scala
@@ -34,12 +34,14 @@ object AppConf {
     def getStorageKey(`type`: String): String = {
         if (`type`.equals("aws")) getConfig("aws_storage_key");
         else if (`type`.equals("azure")) getConfig("azure_storage_key");
+        else if (`type`.equals("cephs3")) getConfig("cephs3_storage_key");
         else "";
     }
 
     def getStorageSecret(`type`: String): String = {
         if (`type`.equals("aws")) getConfig("aws_storage_secret");
         else if (`type`.equals("azure")) getConfig("azure_storage_secret");
+        else if (`type`.equals("cephs3")) getConfig("cephs3_storage_secret");
         else "";
     }
 }
